37c98c8f52899535f71ed4d2d5b15de640dd87a9
Oleg Khristenk <olegkhristenko@gmail.com>
2024-01-23 21:13:38 +0200

    created project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Todo from './components/Todo.vue'
+</script>
+
+<template>
+    <div class="main">
+        Main
+        <Todo />
+    </div>
+</template>
+
+<style scoped lang="sass">
+.main
+    background-color: #545454
+</style>
